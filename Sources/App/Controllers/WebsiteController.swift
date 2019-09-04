d6fad025f51f2978af0fa94178b31b6f262ebfe3
Manas Mishra <manas1.mishra@ril.com>
2019-09-04 20:56:51 +0530

    Creating an acronym with form

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -6,7 +6,11 @@ struct WebsiteController: RouteCollection {
         router.get(use: indexHandler)
         router.get("acronyms", Acronym.parameter, use: acronymHandler)
         router.get("user", UserModel.parameter, use: userHandler)
-        
+        router.get("users", use: allUsersHandler)
+        router.get("categories", use: allCategoriesHandler)
+        router.get("category", Category.parameter, use: categoryHandler)
+        router.get("acronyms", "create", use: createAcronymHandler)
+        router.post(Acronym.self, at: "acronyms", "create", use: createAcronymPostHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -34,6 +38,42 @@ struct WebsiteController: RouteCollection {
         })
     }
     
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        return UserModel.query(on: req).all().flatMap(to: View.self) { (users) in
+            let context = AllUsersContext(title: "Users", users: users)
+            return try req.view().render("users", context)
+        }
+    }
+    
+    func allCategoriesHandler(_ req: Request) throws -> Future<View> {
+        return Category.query(on: req).all().flatMap(to: View.self) { (categories) in
+            let context = AllCategoriesContext.init(title: "Categories", categories: categories)
+            return try req.view().render("categories", context)
+        }
+    }
+    
+    func categoryHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Category.self).flatMap(to: View.self, { (category) in
+            let acronyms = try category.acronyms.query(on: req).all()
+            let context = CategoryContext(title: category.name, acronyms: acronyms)
+            return try req.view().render("category", context)
+        })
+    }
+    
+    func createAcronymHandler(_ req: Request) throws -> Future<View> {
+        let context = CreateAcronymContext(users: UserModel.query(on: req).all())
+        return try req.view().render("createAcronym", context)
+    }
+    
+    func createAcronymPostHandler(_ req: Request, acronym: Acronym) throws -> Future<Response> {
+        return acronym.save(on: req).map { (acronym) in
+            guard let id = acronym.id else {
+                return req.redirect(to: "/")
+            }
+            return req.redirect(to: "/acronyms/\(id)")
+        }
+    }
+    
 }
 
 struct IndexContent: Encodable {
@@ -50,4 +90,23 @@ struct UserContext: Encodable {
     let title: String
     let user: UserModel
     let acronyms: Future<[Acronym]>
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [UserModel]
+}
+struct AllCategoriesContext: Encodable {
+    let title: String
+    let categories: [Category]
+}
+
+struct CategoryContext: Encodable {
+    let title: String
+    let acronyms: Future<[Acronym]>
+}
+
+struct CreateAcronymContext: Encodable {
+    let title = "Create An Acronym"
+    let users: Future<[UserModel]>
 }
